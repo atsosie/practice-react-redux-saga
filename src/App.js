0ff41ea59a8dfb06d13a39306b0ac6a784c8d6ce
Amber Tsosie <atxambra@gmail.com>
2018-07-04 00:03:33 -0700

    Initial commit. Followed steps in tutorial to set things up. Finished everything up to testing the error hhandling.

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { connect } from "react-redux";
+
+class App extends Component {
+  render() {
+    const { fetching, dog, onRequestDog, error } = this.props;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={dog || logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Dog Saga</h1>
+        </header>
+
+        {dog ? (
+          <p className="App-intro">Keep clicking for new dogs</p>
+        ) : (
+          <p className="App-intro">Replace the React icon with a dog!</p>
+        )}
+
+        {fetching ? (
+          <button disabled>Fetching...</button>
+        ) : (
+          <button onClick={onRequestDog}>Request a Dog</button>
+        )}
+
+        {error && <p style={{ color: "red" }}>Uh oh - something went wrong!</p>}
+      </div>
+    );
+  }
+}
+
+// do this to make the most current state of fetching, dog, and error
+// available as props in the App component:
+const mapStateToProps = state => {
+  return {
+    fetching: state.fetching,
+    dog: state.dog,
+    error: state.error
+  };
+};
+
+// this creates a function that dispatches an API_CALL_REQUEST action
+// to the Store:
+const mapDispatchToProps = dispatch => {
+  return {
+    onRequestDog: () => dispatch({ type: "API_CALL_REQUEST" })
+  };
+};
+
+// connect the App component and export this "reduxed" version of it
+// for use in index.js:
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
